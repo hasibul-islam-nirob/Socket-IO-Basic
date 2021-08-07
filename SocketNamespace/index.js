578532dedf4fd99c1a1719862e010f38ce1c5751
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-07 08:59:48 +0600

    Socket IO  -  Socket Namespace  Basic

---
+++ SocketNamespace/index.js
@@ -0,0 +1,65 @@
+const expressJS = require('express');
+const app = expressJS();
+
+const http = require('http');
+const expressJsServer = http.createServer(app);
+
+// Socket IO Configuration
+const {Server} =require('socket.io');
+const io = new Server(expressJsServer);
+
+
+//Create Namespace Group One
+let group1 = io.of("/groupOne");
+group1.on( "connection", function (socket) {
+    group1.emit("group1","Hello Group One");
+})
+
+//Create Namespace Group Two
+let group2 = io.of("/groupTwo");
+group2.on( "connection", function (socket) {
+    group2.emit("group2","Hello Group Two");
+})
+
+
+app.get('/', function (request, response){
+    response.sendFile(__dirname+"/index.html");
+})
+
+
+expressJsServer.listen(3000, function (){
+    console.log("Serveer Run Success on @3000 Port");
+})
+
+
+
+
+/*
+
+//Create Group One
+let group1 = io.of("/groupOne");
+group1.on( "connection", function (socket) {
+    console.log("New Group 1 Connected")
+
+    group1.emit("group1","Hello Group One");
+
+    socket.on("disconnect", function (){
+        console.log("Group 1 Disconnected")
+    })
+})
+
+
+//Create Group Two
+let group2 = io.of("/groupTwo");
+group2.on( "connection", function (socket) {
+    console.log("New Group 2 Connected")
+
+    group2.emit("group2","Hello Group Two");
+
+    socket.on("disconnect", function (){
+        console.log("Group 2 Disconnected")
+    })
+})
+
+
+ */
