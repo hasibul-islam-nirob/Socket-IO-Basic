26f9b9eca152063e3ad63ccbe3069635a2925201
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-08 07:06:50 +0600

    Simple Chat App Complete

--- SimpleRealTimeChatApp/index.js
+++ SimpleRealTimeChatApp/index.js
@@ -1,8 +1,37 @@
 const expressJS = require('express');
-const app = expressJS();
-
 const http = require('http');
+
+const multer = require('multer');
+const multerInc = multer();
+
+const app = expressJS();
 const expressJsServer = http.createServer(app);
+
+
+// Multer Start
+var myStorage = multer.diskStorage({
+    destination:function (request, file, callBack){
+        callBack(null,'./img')
+    },
+    filename:function (request, file, callBack){
+        callBack(null, file.originalname)
+    }
+})
+
+var upload = multer({storage:myStorage}).single('myfile');
+
+app.post("/img", function (request,response){
+
+    upload(request, response, function (error) {
+        if (error){
+            response.send("File Upload Fail");
+        }else {
+            response.send("File Upload Success");
+        }
+    })
+
+})
+//Multer End
 
 // Socket IO Configuration
 const {Server} =require('socket.io');
@@ -13,6 +42,9 @@ io.on( "connection", function (socket) {
     console.log("New User Connected")
 
 
+    socket.on("msgSendServer",function (chatMsg) {
+        io.emit("magTransfer",chatMsg);
+    })
 
 
     socket.on("disconnect", function (){
